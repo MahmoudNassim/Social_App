6f68005abc27f80a8e2fb71c5fefdab920702706
MahmoudNassim <mahmodnassim7@gmail.com>
2023-12-12 18:15:47 +0200

    setup layout completed

--- lib/modules/feeds/feeds_screen.dart
+++ lib/modules/feeds/feeds_screen.dart
@@ -6,12 +6,13 @@ class FeedsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
       child: Container(
         width: double.infinity,
         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
           const Card(
               margin: EdgeInsets.all(10),
-              elevation: 10,
+              elevation: 5,
               clipBehavior: Clip.antiAliasWithSaveLayer,
               child: Stack(
                 alignment: Alignment.bottomRight,
@@ -21,6 +22,7 @@ class FeedsScreen extends StatelessWidget {
                         'https://img.freepik.com/free-photo/photo-overjoyed-hipster-looks-gladfully-away-points-fore-finger-blank-space_273609-25559.jpg?w=900&t=st=1702216902~exp=1702217502~hmac=c4d0b7d1ccbbaa81b7578b4f98e07f74fd84c556422d7ebc0347208f2e61039c'),
                     fit: BoxFit.cover,
                     width: double.infinity,
+                    height: 150,
                   ),
                   Padding(
                     padding: EdgeInsets.all(7.0),
@@ -34,8 +36,19 @@ class FeedsScreen extends StatelessWidget {
                   ),
                 ],
               )),
-          Card(
-            elevation: 7,
+        ListView.separated(
+          separatorBuilder: (context,index)=> const SizedBox(height: 8,),
+          itemBuilder: (context,index)=>buildPostItem(context),
+          itemCount: 10,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+        )
+        ]),
+      ),
+    );
+  }
+  Widget buildPostItem(context)=> Card(
+            elevation: 5,
             color: Colors.white,
             clipBehavior: Clip.antiAliasWithSaveLayer,
             margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
@@ -76,7 +89,7 @@ class FeedsScreen extends StatelessWidget {
                             ],
                           ),
                           Text(
-                            '11 December 2023',
+                            'December 11, 2023',
                             style:
                                 Theme.of(context).textTheme.bodySmall!.copyWith(
                                       height: 1.3,
@@ -97,7 +110,7 @@ class FeedsScreen extends StatelessWidget {
                       height: 1,
                     ),
                   ),
-                  Text(
+                  const Text(
                     'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting .',
                     style: TextStyle(
                       height: 1.3,
@@ -113,12 +126,12 @@ class FeedsScreen extends StatelessWidget {
                           padding: EdgeInsets.zero,
                           minWidth: 1,
                           onPressed: () {},
-                          child: Text(
+                          child: const Text(
                             '#Flutter_Developer ',
                             style: TextStyle(color: Colors.blue),
                           ),
                         ),
-                      ),                      
+                      ),
                       Container(
                         height: 25,
                         child: MaterialButton(
@@ -126,12 +139,12 @@ class FeedsScreen extends StatelessWidget {
                           padding: EdgeInsets.zero,
                           minWidth: 1,
                           onPressed: () {},
-                          child: Text(
+                          child: const Text(
                             '#Flutter_Developer ',
                             style: TextStyle(color: Colors.blue),
                           ),
                         ),
-                      ),                      
+                      ),
                       Container(
                         height: 25,
                         child: MaterialButton(
@@ -139,12 +152,12 @@ class FeedsScreen extends StatelessWidget {
                           padding: EdgeInsets.zero,
                           minWidth: 1,
                           onPressed: () {},
-                          child: Text(
+                          child: const Text(
                             '#Flutter_Developer ',
                             style: TextStyle(color: Colors.blue),
                           ),
                         ),
-                      ),                      
+                      ),
                       Container(
                         height: 25,
                         child: MaterialButton(
@@ -152,12 +165,12 @@ class FeedsScreen extends StatelessWidget {
                           padding: EdgeInsets.zero,
                           minWidth: 1,
                           onPressed: () {},
-                          child: Text(
+                          child: const Text(
                             '#Flutter_Developer ',
                             style: TextStyle(color: Colors.blue),
                           ),
                         ),
-                      ),                      
+                      ),
                       Container(
                         height: 25,
                         child: MaterialButton(
@@ -165,33 +178,135 @@ class FeedsScreen extends StatelessWidget {
                           padding: EdgeInsets.zero,
                           minWidth: 1,
                           onPressed: () {},
-                          child: Text(
+                          child: const Text(
                             '#Flutter_Developer ',
                             style: TextStyle(color: Colors.blue),
                           ),
                         ),
-                      ),                      
+                      ),
                     ],
                   ),
                   Container(
                     height: 120,
                     width: double.infinity,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        image:NetworkImage(
-                          'https://img.freepik.com/free-photo/photo-overjoyed-hipster-looks-gladfully-away-points-fore-finger-blank-space_273609-25559.jpg?w=900&t=st=1702216902~exp=1702217502~hmac=c4d0b7d1ccbbaa81b7578b4f98e07f74fd84c556422d7ebc0347208f2e61039c'
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                            image: NetworkImage(
+                                'https://img.freepik.com/free-photo/photo-overjoyed-hipster-looks-gladfully-away-points-fore-finger-blank-space_273609-25559.jpg?w=900&t=st=1702216902~exp=1702217502~hmac=c4d0b7d1ccbbaa81b7578b4f98e07f74fd84c556422d7ebc0347208f2e61039c'),
+                            fit: BoxFit.cover)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: InkWell(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 5),
+                              child: Row(
+                                children: [
+                                  const Icon(
+                                    Icons.favorite_border,
+                                    size: 18,
+                                    color: Colors.red,
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    '120',
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            onTap: () {},
                           ),
-                           fit: BoxFit.cover
-                        )
+                        ),
+                        Expanded(
+                          child: InkWell(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 5),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  const Icon(
+                                    Icons.messenger_outline,
+                                    size: 18,
+                                    color: Colors.amber,
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    '1200 comments',
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            onTap: () {},
+                          ),
+                        ),
+                      ],
                     ),
-                  )
+                  ),
+                  Container(
+                    color: Colors.grey[400],
+                    width: double.infinity,
+                    height: 1,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: InkWell(
+                          onTap: (){},
+                          child: Row(
+                            children: [
+                              const CircleAvatar(
+                                radius: 18,
+                                backgroundImage: NetworkImage(
+                                    'https://img.freepik.com/free-photo/3d-rendering-ice-hockey-player_23-2150898762.jpg?t=st=1702306368~exp=1702309968~hmac=9cf561992fce46b219a185f768204cb8c84cd73bc0c74b9df9a1c45d3e881dc2&w=740'),
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              Text('write a comment...',
+                                  style: Theme.of(context).textTheme.bodySmall!),
+                            ],
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.favorite_border,
+                              size: 18,
+                              color: Colors.red,
+                            ),
+                            const SizedBox(
+                              width: 3,
+                            ),
+                            Text(
+                              'Like',
+                              style: Theme.of(context).textTheme.bodySmall,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ],
               ),
             ),
-          ),
-        ]),
-      ),
-    );
-  }
+          );
+          
 }
